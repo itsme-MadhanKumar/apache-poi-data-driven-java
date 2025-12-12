0d378fcbf59fa93199b2ce49482218139bd77a68
Madhan Kumar B <madhandivine3@gmail.com>
2025-12-12 19:37:34 +0530

    second commit

--- src/test/java/org/example/Datadriven.java
+++ src/test/java/org/example/Datadriven.java
@@ -1,8 +1,11 @@
 package org.example;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Iterator;
 public class Datadriven
 {
     public static void main(String[] args) throws IOException
@@ -18,8 +21,41 @@ public class Datadriven
             String name  = workbook.getSheetName(i);
             if(name.equalsIgnoreCase("testdata"))
             {
-               XSSFSheet sheet =  workbook.getSheetAt(i);
+               XSSFSheet sheet =  workbook.getSheetAt(i); // setting sheet to work book
+               Iterator<Row> rows = sheet.iterator(); // getting all rows using iterator
+               Row row = rows.next(); // moving to 1st row
+                Iterator<Cell> cell =  row.cellIterator(); // in that 1st row getting all cells
+                int k = 0;
+                int coloum = 0;
+                while (cell.hasNext())
+                {
+                    Cell value = cell.next(); // moving to 1st cell
+                    String coloumname = value.getStringCellValue(); // getting current cell name
+                    if(coloumname.equalsIgnoreCase("Testcases"))
+                    {
+                        //get data
+                        coloum = k;
+                    }
+                    k++;
+                }
+                System.out.println(coloum);
+
+//                =============================
+                while (rows.hasNext())
+                {
+                   Row r = rows.next();
+                   if(r.getCell(coloum).getStringCellValue().equalsIgnoreCase("Purchase"))
+                   {
+                       Iterator<Cell> cv = r.cellIterator();
+                       while (cv.hasNext())
+                       {
+                           System.out.println(cv.next().getStringCellValue());
+                       }
+                   }
+                }
+
             }
+
         }
     }
 }
